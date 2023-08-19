587604ecd65dbec8ec0947086d46f1c584250f7e
poker555 <brad901017@gmail.com>
2023-08-19 23:02:28 +0800

    add silver block

--- src/main/java/com/PokerMod/pokerfirstmod/Mymod.java
+++ src/main/java/com/PokerMod/pokerfirstmod/Mymod.java
@@ -1,5 +1,8 @@
 package com.PokerMod.pokerfirstmod;
 
+import com.PokerMod.pokerfirstmod.block.ModBlocks;
+import com.PokerMod.pokerfirstmod.item.ModCreativeModeTabs;
+import com.PokerMod.pokerfirstmod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
@@ -25,73 +28,60 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Mymod.MODID)
+@Mod(Mymod.MOD_ID)
 public class Mymod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "pokerfirstmod";
+    public static final String MOD_ID = "pokerfirstmod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
     public Mymod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
+        ModBlocks.register(modEventBus);
+
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
+
         modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
     }
 
     private void addCreative(CreativeModeTabEvent.BuildContents event)
     {
-        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
-    }
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SILVER);
+        }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.SILVER_BLOCK);
+        }
+
+        if (event.getTab() == ModCreativeModeTabs.INDUSTRY_TAB){
+            event.accept(ModItems.SILVER);
+            event.accept(ModBlocks.SILVER_BLOCK);
+        }
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
         }
     }
 }
---
+++ src/main/java/com/PokerMod/pokerfirstmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,21 @@
+package com.PokerMod.pokerfirstmod.item;
+
+import com.PokerMod.pokerfirstmod.Mymod;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Mymod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModCreativeModeTabs {
+    public static CreativeModeTab INDUSTRY_TAB;
+
+    @SubscribeEvent
+    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event){
+        INDUSTRY_TAB = event.registerCreativeModeTab(new ResourceLocation(Mymod.MOD_ID,"industry_tab")
+        , builder -> builder.icon(() -> new ItemStack(ModItems.SILVER.get())).title(Component.translatable("creativemodetab.industry_tab")));
+    }
+}
